a4d1e307e2e03fb87c890482f7175845971641ab
cryswerton <cryswertons@gmail.com>
2023-10-08 16:18:49 +0100

    it shows the user name on the dashboard page

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,23 @@
 <template>
     <h1>Dashboard</h1>
+    <div v-if="name.length > 0">
+        <p>You are logged in as {{ name }}</p>
+    </div>
 </template>
+
+<script>
+import { useAuth } from '@/stores/auth.js'
+
+export default {
+    data(){
+        return {
+            name: ''
+        }
+    },
+    mounted() {
+        const auth = useAuth()
+        const user = JSON.parse(auth.user)
+        this.name = user.name
+    }
+}
+</script>
